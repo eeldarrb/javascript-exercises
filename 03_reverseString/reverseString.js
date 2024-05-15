044e1dd1fb78af018d338c5533d5364b07cc0708
bradley <167944058+eeldarrb@users.noreply.github.com>
2024-05-15 18:48:58 -0400

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let reversedString = "";
+  for (let i = str.length; i > 0; i--) {
+    reversedString += str.charAt(i-1); 
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
