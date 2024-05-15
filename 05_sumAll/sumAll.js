2fc2e13a7adbb3897131b80b8c3db490974a0c17
bradley <167944058+eeldarrb@users.noreply.github.com>
2024-05-15 19:34:51 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (intA, intB) {
+  if (typeof intA != "number" || typeof intB != "number" || (intA || intB) < 0) return "ERROR";
+  let sum = 0;
+  let larger = intA;
+  let smaller = intB;
 
+  if (intB > intA) {
+    larger = intB;
+    smaller = intA;
+  }
+  for (let i = smaller; i <= larger; i++) sum += i;
+  return sum;
 };
 
 // Do not edit below this line
