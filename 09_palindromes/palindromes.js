77ebe2945a765542569cb362d1811db285a75d5f
bradley <167944058+eeldarrb@users.noreply.github.com>
2024-05-30 21:50:06 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let letters = word
+        .replace(/[^a-zA-Z1-9]+/g,"")
+        .toLowerCase()
+        .split("");
 
+    console.log(letters);
+    for (let i = 0; i < letters.length/2; i++) {
+        console.log(letters[i]);
+        console.log(letters[letters.length-i-1]);
+        if (letters[i] != letters[letters.length-i-1]) return false;
+    }
+    return true;
 };
 
+palindromes("test  a33!");
 // Do not edit below this line
 module.exports = palindromes;
