f042003156dcd5aefe7f2801681d2428989bec46
bradley <167944058+eeldarrb@users.noreply.github.com>
2024-06-02 14:00:30 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.map((person) => {
+    if ("yearOfDeath" in person == false)
+      person["yearOfDeath"] = new Date().getFullYear();
+  });
+  people.sort((a, b) => {
+    aAge = a.yearOfDeath - a.yearOfBirth;
+    bAge = b.yearOfDeath - b.yearOfBirth;
+    return bAge - aAge;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
